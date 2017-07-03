829a5bb42633125f5b21f4ee5fd5ff4868ce354d
FernandoDurier <fernando.durier@hotmail.com>
2017-07-03 20:42:50 -0300

    adding a global setup pip install

--- dev/flask/Scripts/activate.bat
+++ dev/flask/Scripts/activate.bat
@@ -1,32 +1,30 @@
-@echo off
-set "VIRTUAL_ENV=C:\Users\IBM_ADMIN\Documents\Python API development\dev\flask"
-
-if not defined PROMPT (
-    set "PROMPT=$P$G"
-)
-
-if defined _OLD_VIRTUAL_PROMPT (
-    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
-)
-
-if defined _OLD_VIRTUAL_PYTHONHOME (
-    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
-)
-
-set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
-set "PROMPT=(flask) %PROMPT%"
-
-if defined PYTHONHOME (
-    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
-    set PYTHONHOME=
-)
-
-if defined _OLD_VIRTUAL_PATH (
-    set "PATH=%_OLD_VIRTUAL_PATH%"
-) else (
-    set "_OLD_VIRTUAL_PATH=%PATH%"
-)
-
-set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
-
-:END
+@echo off
+set "VIRTUAL_ENV=C:\Users\user\DOCUME~1\PERSON~1\Python\FLASKL~1\MICROB~1\FLASK-~1\dev\flask"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+    set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+)
+set "PROMPT=(flask) %PROMPT%"
+
+REM Don't use () to avoid problems with them in %PATH%
+if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
+
+set PYTHONHOME=
+
+REM if defined _OLD_VIRTUAL_PATH (
+if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+:ENDIFVPATH1
+REM ) else (
+if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
